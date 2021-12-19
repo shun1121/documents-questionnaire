071b882ed14076923ee139dc6bf0b46a9177d903
shun1121 <hikitanikun.ware80000@gmail.com>
2021-12-20 00:02:20 +0900

    connected selectbox with reduxtoolkit

--- src/components/selectBox.tsx
+++ src/components/selectBox.tsx
@@ -1,18 +1,28 @@
-import React, {useState} from "react"
+import React, { VFC, useState } from "react"
 import { timeOptions } from "./selectBoxOptions"
+import { useDispatch, useSelector } from "react-redux"
+import { selectBox, selectVal } from "../features/questionnaire/questionnaireSlice"
 
-const SelectBox = (props) => {
+type Props = {
+  time: string;
+}
+
+const SelectBox: VFC<Props> = (props) => {
   const [selectValue, setSelectValue] = useState("")
+  const dispatch = useDispatch()
+  const selectedValue = useSelector(selectVal)
   const options = timeOptions
 
   const handleChange = (e: any) => {
-    setSelectValue(e.target.value)
+    const selectVal = e.target.value
+    setSelectValue(selectVal)
+    dispatch(selectBox(selectVal))
   }
   console.log(selectValue)
 
   return (
     <>
-      <select name="time" value={selectValue} onChange={handleChange}>
+      <select name="time" value={selectedValue} onChange={handleChange}>
         <option hidden>時間を選択してください。</option>
         {options.map((option) => (
           <option key={option[0]} value={option[1]}>
