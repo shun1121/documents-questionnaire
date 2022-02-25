8db7a97bb6586388ecbcae67841bd2ffa7de4e56
shun1121 <hikitanikun.ware80000@gmail.com>
2022-02-25 18:38:01 +0900

    added userEvent

--- src/components/inputText/inputText.test.tsx
+++ src/components/inputText/inputText.test.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
-import { InputText } from './inputText'
+import userEvent from '@testing-library/user-event'
 import { Provider } from 'react-redux'
 import { store } from '../../app/store'
+import { StoryInput } from './storyInput'
 
 describe('input test', () => {
   it('input test', () => {
+    const handleChange = jest.fn();
+    const text = "you're goddamn right!"
     render(
       <Provider store={store}>
-        <InputText />
+        <StoryInput text={text} handleChange={handleChange} />
       </Provider>
     )
-    const inputValue = screen.getByRole("textbox");
-    fireEvent.change(inputValue, { target: { value: "you're goddamn right!" }});
-    expect(inputValue).toHaveValue("you're goddamn right!");
+    userEvent.type(screen.getByRole('textbox'), "you're goddamn right!");
+    expect(screen.getByRole('textbox')).toHaveValue("you're goddamn right!")
+    expect(handleChange).toHaveBeenCalledTimes(21);
   })
 })
